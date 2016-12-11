aa42ead524060ea14f7140a6bdd3654b646d14db
emiliocampa-hoyos <campa813@gmail.com>
2016-12-10 22:52:01 -0600

    Added comments for Web Profe to read.

--- SitioCenterCo/php/__UserQueryHelper.php
+++ SitioCenterCo/php/__UserQueryHelper.php
@@ -2,7 +2,11 @@
 class __UserQueryHelper {
 
 
-
+	/**
+	* This method does what it says, it inserts a username and a password
+	* using the given connection. It used prepared statements in order
+	* to perform a syntax check on the variables passed (safe for SQL injection).
+	*/
 	public static function insertUser($connection, $username, $password) {
 		$success = false;
 
--- SitioCenterCo/php/create_database.php
+++ SitioCenterCo/php/create_database.php
@@ -10,11 +10,15 @@ require("__Redirect.php");
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+    //Create database connection
     $myConnection = new DatabaseConnection;
     $myConnection->createConnection();
 
+    //Call static method from __UserQueryHelper by passing the connection, username and password
+    //Save the result from it on variable success, either 1 or -1
     $success = (__UserQueryHelper::insertUser($myConnection->getConnection(), $username, $password) ? 1 : -1);
  }
+ //Call redirect to home method from __Redirect class
  __Redirect::toHome("?success="+$success);
 
  exit();
@@ -23,7 +27,10 @@ require("__Redirect.php");
 
 
 
-
+ /**
+ * This method validates if the submit button, the username and the password
+ * were correctly set, if they are, then it returns true else false 
+ */
  function validInput() {
     $submittedButton = $_POST['submit'];
     $username = $_POST['username'];
